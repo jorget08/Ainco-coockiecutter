a0e2abea68e208a1f2c29a1c20c88528095d018b
Jorge Torres <jorge.torres@antpack.co>
2023-05-25 09:24:55 -0500

    first commit

---
+++ ainco/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from ainco.users.models import User
+from ainco.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
